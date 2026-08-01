595f5d773821bcd35ea33fb1f84f9188f596f742
Duckie <stylianouandreas69@gmail.com>
2026-08-01 14:35:01 +0300

    forgot to remove this and also to add this file

--- python/championship.py
+++ python/championship.py
@@ -6,13 +6,21 @@ teams=["A", "B", "C", "D", "E", "F", "G", "H","I", "J"]
 group1=random.sample(teams, 5)
 group2=random.sample([team for team in teams if team not in group1], 5)
 semi_finalists=[]
+d=[0]*5
+y=[0]*5
+points_23=[0]*5
+points_12=[0]*5
 
 for i in range(5):
-    winner=random.choice([group1[i], group2[i]])
-    points_1=3 if winner==group1[i] else 0
-    points_2=3 if winner==group2[i] else 0
-    print(f"Group 1: {group1[i]} vs Group 2: {group2[i]} -> Winner: {winner} (Points: {points_1, points_2})\n")
-    sleep(1.3)
+        winner=random.choice([group1[i], group2[i]])
+        points_1=3 if winner==group1[i] else 0
+        d[i]=points_1
+        points_23[i]+=points_1
+        points_2=3 if winner==group2[i] else 0
+        y[i]=points_2
+        points_12[i]+=points_2
+        print(f"Group 1: {group1[i]} vs Group 2: {group2[i]} -> Winner: {winner} (Points: {d[i], y[i]})\n")
+        sleep(1.3)
     
 semi_finalists_1=heapq.nlargest(2, group1)
 semi_finalists_2=heapq.nlargest(2, group2)
