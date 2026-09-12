dd5480a593b08bf5ca7c2ab4e0cea20769e97ca1
Duckie <stylianouandreas69@gmail.com>
2026-09-12 23:08:37 +0300

    Just added a try and except block for edge cases:3

--- src/coding_stuff_ig/python/guessing_game.py
+++ src/coding_stuff_ig/python/guessing_game.py
@@ -5,29 +5,32 @@ def guessing_game():
     tries = 0
 
     while True:
-        guess_number = int(input("Guess the random number from 1 through 50\n"))
+        try:
+            guess_number = int(input("Guess the random number from 1 through 50\n"))
 
-        if guess_number > 50 or guess_number < 1:
-            print("Invalid number. Try again")
-            continue
+            if guess_number > 50 or guess_number < 1:
+                print("Invalid number. Try again")
+                continue
 
-        tries += 1
+            tries += 1
 
-        if number_to_guess == guess_number:
-            print("Congrats! You found the number!")
-            print(f"It took you {tries} tries")
-            break
+            if number_to_guess == guess_number:
+                print("Congrats! You found the number!")
+                print(f"It took you {tries} tries")
+                break
 
-        if abs(number_to_guess - guess_number) <= 5:
-            print("Hotter! Try again!\n")
-        else:
-            print("Colder! Try again!")
+            if abs(number_to_guess - guess_number) <= 5:
+                print("Hotter! Try again!\n")
+            else:
+                print("Colder! Try again!")
 
-    try_again = input("Ya wanna try again? (yes/no): ").lower()
-    if try_again == "yes":
-        print("Sweet! Good luck:3")
-        guessing_game()
-    else:
-        print("Alright:3 Bye!")
+            try_again = input("Ya wanna try again? (yes/no): ").lower()
+            if try_again == "yes":
+                print("Sweet! Good luck:3")
+                guessing_game()
+            else:
+                print("Alright:3 Bye!")
+        except ValueError:
+            print("Wrong input! Try again!")
 
-guessing_game()       
+guessing_game()
